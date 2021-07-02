58352010ba3c67f6c03ba042764b410c1519dff4
Robert Crocker <robert@vizsimply.com>
2021-07-01 21:02:34 -0500

    Able to eat the Apple.

--- src/Apple.js
+++ src/Apple.js
@@ -1,28 +1,30 @@
 /*
-- [ ] The apple should appear randomly on the screen
-- [ ] The apple should appear within the size of the board
+- [x] The apple should appear randomly on the screen
+- [x] The apple should appear within the size of the board
 - [ ] Remove the apple when the head of the snake reaches the apple
 - [ ] Another apple should appear on the screen
 - [ ] The apple should not appear on the snake
 */
 export class Apple {
-  constructor(el) {
+  constructor(el, position) {
+    console.log("Inside Apple ", position)
     this.node = document.createElement("img")
     this.node.setAttribute("id", "apple")
     this.node.setAttribute("src", "src/assets/apple.jpg")
 
     el.appendChild(this.node)
 
-    const randPos = Math.floor((Math.random() * 700) / 50) * 50
+    //const randPos = Math.floor((Math.random() * 700) / 50) * 50
 
-    this.node.style.left = `${randPos}px`
-    this.node.style.top = `${randPos}px`
-    console.log(randPos, "RS")
+    this.node.style.left = `${position}px`
+    this.node.style.top = `${position}px`
 
     //apple picking
     const appleImg = document.getElementById("apple")
-    console.log(appleImg)
+    //console.log(appleImg)
     //appleImg.style.margin
     //delete appleImg => grabbing img so that works
+
+    console.log(head, "head")
   }
 }
--- src/Head.js
+++ src/Head.js
@@ -1,5 +1,5 @@
 export class Head {
-  constructor(el) {
+  constructor(el, position) {
     this.node = document.createElement("div")
     this.node.setAttribute("id", "head")
 
@@ -10,41 +10,42 @@ export class Head {
 
     this.node.style.top = 0
     this.node.style.left = 0
+    console.log(position, "pos in cx")
 
-    setTimeout(this.move.bind(this), this.SPEED)
+    setTimeout(this.move.bind(this, el, position), this.SPEED)
   }
 
-  move() {
+  move(el, position) {
     const head = this.node
     let direction = this.currentDirection
 
-    let topPosition = Number(head.style.top.replace("px", ""))
-    let leftPosition = Number(head.style.left.replace("px", ""))
+    let y = Number(head.style.top.replace("px", ""))
+    let x = Number(head.style.left.replace("px", ""))
+
+    if (x === position && y === position) {
+      const appleImg = document.getElementById("apple")
+      el.removeChild(appleImg)
+    }
 
     // Bonus: Head shouldn't be able to go backwards
     switch (true) {
       case direction === "up":
-        head.style.top = `${(topPosition -= 50)}px`
+        head.style.top = `${(y -= 50)}px`
         break
       case direction === "right":
-        head.style.left = `${(leftPosition += 50)}px`
+        head.style.left = `${(x += 50)}px`
         break
       case direction === "down":
-        head.style.top = `${(topPosition += 50)}px`
+        head.style.top = `${(y += 50)}px`
         break
       case direction === "left":
-        head.style.left = `${(leftPosition -= 50)}px`
+        head.style.left = `${(x -= 50)}px`
         break
       default:
         throw new Error("")
     }
 
-    if (
-      topPosition < 0 ||
-      topPosition >= 700 ||
-      leftPosition >= 700 ||
-      leftPosition < 0
-    ) {
+    if (y < 0 || y >= 700 || x >= 700 || x < 0) {
       console.log("Triggered")
       Number(head.style.top.replace("px", ""))
       Number(head.style.left.replace("px", ""))
@@ -53,6 +54,7 @@ export class Head {
       this.currentDirection = "right"
     }
 
-    setTimeout(this.move.bind(this), this.SPEED)
+    console.log(position, "position in move")
+    setTimeout(this.move.bind(this, el, position), this.SPEED)
   }
 }
