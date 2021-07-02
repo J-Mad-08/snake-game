0953dd462ebac0d6a47f58b2127986f9751134e9
Robert Crocker <robert@vizsimply.com>
2021-07-01 21:43:09 -0500

    Randomly positioning the Apple while avoiding placing the Apple on the Head.

--- src/Apple.js
+++ src/Apple.js
@@ -1,8 +1,8 @@
 /*
 - [x] The apple should appear randomly on the screen
 - [x] The apple should appear within the size of the board
-- [ ] Remove the apple when the head of the snake reaches the apple
-- [ ] Another apple should appear on the screen
+- [x] Remove the apple when the head of the snake reaches the apple
+- [x] Another apple should appear on the screen
 - [ ] The apple should not appear on the snake
 */
 export class Apple {
@@ -14,17 +14,14 @@ export class Apple {
 
     el.appendChild(this.node)
 
-    //const randPos = Math.floor((Math.random() * 700) / 50) * 50
-
     this.node.style.left = `${position}px`
     this.node.style.top = `${position}px`
 
     //apple picking
-    const appleImg = document.getElementById("apple")
+    // const appleImg = document.getElementById("apple")
     //console.log(appleImg)
     //appleImg.style.margin
     //delete appleImg => grabbing img so that works
-
-    console.log(head, "head")
+    // console.log(head, "head")
   }
 }
--- src/Head.js
+++ src/Head.js
@@ -1,3 +1,5 @@
+import { Apple } from "./Apple.js"
+
 export class Head {
   constructor(el, position) {
     this.node = document.createElement("div")
@@ -10,7 +12,6 @@ export class Head {
 
     this.node.style.top = 0
     this.node.style.left = 0
-    console.log(position, "pos in cx")
 
     setTimeout(this.move.bind(this, el, position), this.SPEED)
   }
@@ -22,9 +23,19 @@ export class Head {
     let y = Number(head.style.top.replace("px", ""))
     let x = Number(head.style.left.replace("px", ""))
 
+    // Does our Apple position equal our Snake
     if (x === position && y === position) {
       const appleImg = document.getElementById("apple")
       el.removeChild(appleImg)
+      // Create a new random position to place our Apple
+      let randPos = Math.floor((Math.random() * 700) / 50) * 50
+      //if next apple is on snake, call random again
+      while (x === randPos && y === randPos) {
+        randPos = Math.floor((Math.random() * 700) / 50) * 50
+      }
+      new Apple(el, randPos)
+      // Let our Head know of the new Apple position
+      position = randPos
     }
 
     // Bonus: Head shouldn't be able to go backwards
@@ -46,7 +57,6 @@ export class Head {
     }
 
     if (y < 0 || y >= 700 || x >= 700 || x < 0) {
-      console.log("Triggered")
       Number(head.style.top.replace("px", ""))
       Number(head.style.left.replace("px", ""))
       head.style.top = 0
@@ -54,7 +64,6 @@ export class Head {
       this.currentDirection = "right"
     }
 
-    console.log(position, "position in move")
     setTimeout(this.move.bind(this, el, position), this.SPEED)
   }
 }
